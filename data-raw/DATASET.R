364e134babacd1f1e406fc4983be8a514fffaa8f
ChristopherNClark <chrclark@fiu.edu>
2023-12-05 12:23:48 -0500

    created data folder, edited dataset.r script

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,18 @@
-## code to prepare `DATASET` dataset goes here
+#' Prepare and save the DATASET dataset
+#'
+#' @noRd
+#'
 
+# Load necessary libraries
+#' @importFrom arrow read_parquet
+#' @importFrom usethis use_data
+
+# Construct the GitHub raw file URLs
+file_names <- c("HPS_week1_data_parquet.parquet", "HPS_week2_data_parquet.parquet")
+github_urls <- paste0("https://github.com/ChristopherNClark/hpsro/raw/master/data-raw/", file_names)
+
+# Read data from GitHub using arrow::read_parquet
+DATASET <- purrr::map_df(github_urls, arrow::read_parquet)
+
+# Use usethis::use_data to save DATASET in the data folder
 usethis::use_data(DATASET, overwrite = TRUE)

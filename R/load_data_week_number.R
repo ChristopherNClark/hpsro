463fb771b6cddefc3bffabdf79474413a2db2784
ChristopherNClark <chrclark@fiu.edu>
2023-12-05 13:20:16 -0500

    Edited function

--- R/load_data_week_number.R
+++ R/load_data_week_number.R
@@ -5,6 +5,7 @@
 #' @return data
 #' @export
 #' @importFrom arrow read_parquet
+#' @importFrom utils data
 #'
 #' @examples
 #' \dontrun{
@@ -14,13 +15,9 @@
 load_data_week_number <- function(week) {
   stopifnot(week %in% c(1, 2))
 
-  # Check if data is loaded in the package environment
-  if (!exists("DATASET", envir = asNamespace("hpsro"))) {
-    # If not, load the data using data()
-    data(DATASET, package = "hpsro")
-  }
-
-  # Depending on the structure of your DATASET object, you might need additional code here
-
-  return(DATASET)
+  # Load the data using data() from the data folder
+  data(DATASET, package = "hpsro")
 }
+
+# No need for an 'export()' call here
+

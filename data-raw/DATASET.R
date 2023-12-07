691025a98883878ce2dc3e181277abccd26650d6
ChristopherNClark <chrclark@fiu.edu>
2023-12-07 11:22:45 -0500

    Further edits on data loading

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -26,15 +26,15 @@
 # Week 1:
 parquet_url_week1 <- "https://github.com/prasadbhoite/HPS_PUF/raw/main/Data/01.Week1_04.23.20_05.01.20/HPS_week1_data_parquet.parquet"
 # Read the Parquet file into a data frame
-parquet_data_week1 <- arrow::read_parquet(parquet_url_week1)
-usethis::use_data(parquet_data_week1, overwrite = TRUE)
-save(parquet_data_week1, file = "data/week1.rda")
+data_week1 <- arrow::read_parquet(parquet_url_week1)
+usethis::use_data(data_week1, overwrite = TRUE)
+#save(parquet_data_week1, file = "data/week1.rda")
 
 # Week 2:
 parquet_url_week2 <- "https://github.com/prasadbhoite/HPS_PUF/raw/main/Data/02.Week2_05.07.20_05.12.20/HPS_week2_data_parquet.parquet"
 # Read the Parquet file into a data frame
-parquet_data_week2 <- arrow::read_parquet(parquet_url_week2)
-usethis::use_data(parquet_data_week2, overwrite = TRUE)
-save(parquet_data_week2, file = "data/week2.rda")
+data_week2 <- arrow::read_parquet(parquet_url_week2)
+usethis::use_data(data_week2, overwrite = TRUE)
+#save(parquet_data_week2, file = "data/week2.rda")
 
 
